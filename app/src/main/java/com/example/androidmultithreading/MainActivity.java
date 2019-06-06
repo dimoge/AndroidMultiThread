4f8230cc36b801c8c2c8d667d25fedec5fe1ba1c
guohao <504575890@qq.com>
2019-06-06 10:09:04 +0800

    asyncTask多线程

--- app/src/main/java/com/example/androidmultithreading/MainActivity.java
+++ app/src/main/java/com/example/androidmultithreading/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.androidmultithreading;
 
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -60,5 +65,55 @@ public class MainActivity extends AppCompatActivity {
         thread.start();
 
 
+        getSupportLoaderManager().initLoader(0, null, new LoaderManager.LoaderCallbacks<Object>() {
+            @NonNull
+            @Override
+            public Loader<Object> onCreateLoader(int i, @Nullable Bundle bundle) {
+                return null;
+            }
+
+            @Override
+            public void onLoadFinished(@NonNull Loader<Object> loader, Object o) {
+
+            }
+
+            @Override
+            public void onLoaderReset(@NonNull Loader<Object> loader) {
+
+            }
+        });
+
+
     }
+
+    public class MyAsyncTask extends AsyncTask<String, Integer, Void>{
+
+        @Override
+        protected Void doInBackground(String... voids) {
+            publishProgress(1);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+    }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+
+        }
+    };
 }
